6e4be62dd23819e8f8699d1a778d43417e919ab7
nocomenty7 <nocomenty7@gmail.com>
2026-08-17 21:20:15 +0900

    build: force entrypoint asset renaming with v2 prefix for CDN cache busting

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-v2-[hash].js',
+        chunkFileNames: 'assets/[name]-v2-[hash].js',
+        assetFileNames: 'assets/[name]-v2-[hash].[ext]',
+      }
+    }
+  }
 })
